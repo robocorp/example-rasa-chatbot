ba2074f40ee26bc15ff91602752cd04934f7e84b
Tommi Holmgren <tommi.holmgren@gmail.com>
2022-07-28 10:00:21 +0300

    Changed to use RPA process library on actions, and updated to rasa 3.1

--- actions/actions.py
+++ actions/actions.py
@@ -1,11 +1,11 @@
 import requests
 import spacy
 import settings
+from RPA.Robocorp.Process import Process
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
 
 class ActionHelloWorld(Action):
 
@@ -16,7 +16,7 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text=f"Right! This might take a while as I'll open a browser and check google for you. Don't hang up on me.")
+        # dispatcher.utter_message(text=f"Right! This might take a while as I'll open a browser and check google for you. Don't hang up on me.")
 
         userid = tracker.current_state()['sender_id']
 
@@ -29,20 +29,18 @@ class ActionHelloWorld(Action):
         city = doc.ents[0]
 
         # ---- Start process with single work item payload
-        
-        # Use the demo workspace
-        url = "https://api.eu1.robocorp.com/process-v1/workspaces/d6b65aa4-0c45-4fd7-8bec-d68a29896e78/processes/a4fee379-ee93-4ce7-a64a-842c2677df43/runs?"
-        headers = {"Authorization": settings.control_room_apikey}
-         
+
         # Create work item payload
         data = {
             "user": userid,
-            "city": str(city) 
+            "city": str(city)
         }
 
-        r = requests.post(url, json=data, headers=headers)
+        # Authenticate with Robocorp Control Room API
+        process = Process(settings.workspace, settings.process, settings.apikey)
+        r = process.start_process(data)
 
-        dispatcher.utter_message(text=f"You are user {userid}. I've started a robot to get you an answer. Technical details here: {r.text}")
+        dispatcher.utter_message(text=f"Right, {str(city)} it is. Hold on, robot is running!")
 
         return []
 
